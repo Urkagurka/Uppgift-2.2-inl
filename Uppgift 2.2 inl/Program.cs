05858c33cb3e73b4a55deed9d0a850346b7c0dcb
alve06001 <alve06001@E-PW0234LA>
2023-09-15 13:13:39 +0200

    Add project files.

---
+++ Uppgift 2.2 inl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("hur många meter hoppade elin?");
+            int meter_elin=int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många meter hoppade Alma?");
+            int meter_alma=int.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Elin hoppade " + (meter_elin - meter_alma) + " mer än alma");
+
+
+
+
+
+        }
+    }
+}
